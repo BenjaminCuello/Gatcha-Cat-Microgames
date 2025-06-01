021d20b6595fac52c3834d40329da1b202631507
BrancoAbalos <branco.abalos@alumnos.ucn.cl>
2025-06-01 16:35:45 -0400

    sistema funcionando
    los habia dejado cmo comentarios kjdfjkdf pero ya ta bn

--- v5/scripts/juego.gd
+++ v5/scripts/juego.gd
@@ -7,17 +7,17 @@ var max_microjuegos = 30
 
 # Lista de microjuegos disponibles
 var lista_microjuegos = [
-	#"res://microjuegos/1_Escondite_gatuno.tscn",
-	"res://microjuegos/9_Flappy_cat.tscn"
-	#"res://microjuegos/10_Cartas.tscn",
-	#"res://microjuegos/11_Morder_al_muñeco.tscn",
+	"res://microjuegos/1_Escondite_gatuno.tscn",
+	"res://microjuegos/9_Flappy_cat.tscn",
+	"res://microjuegos/10_Cartas.tscn",
+	"res://microjuegos/11_Morder_al_muñeco.tscn",
 	#"res://microjuegos/12_Gato_lavarse.tscn", falta conectarlo con victoria derrota al sistema
-	#"res://microjuegos/16_Tecla_gatuna.tscn",
-	#"res://microjuegos/18_Gato_equilibrio.tscn",
-	#"res://microjuegos/19_Ronroneo_ritmico.tscn",
-	#"res://microjuegos/21_Abrelatas.tscn",
-	#"res://microjuegos/22_Atrapalo.tscn",
-	#"res://microjuegos/26_Atrapar_raton.tscn"
+	"res://microjuegos/16_Tecla_gatuna.tscn",
+	"res://microjuegos/18_Gato_equilibrio.tscn",
+	"res://microjuegos/19_Ronroneo_ritmico.tscn",
+	"res://microjuegos/21_Abrelatas.tscn",
+	"res://microjuegos/22_Atrapalo.tscn",
+	"res://microjuegos/26_Atrapar_raton.tscn"
 ]
 # Elegir uno al azar
 func obtener_microjuego_aleatorio() -> String:
