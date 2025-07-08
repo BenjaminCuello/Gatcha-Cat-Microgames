2aa42f768af90d05736683026110e433be937d6f
BenjaminCuello <benjamin.cuello@alumnos.ucn.cl>
2025-07-08 11:00:27 -0400

    f

--- v7/EscenasGenerales/Menus/MenuPrincipal.gd
+++ v7/EscenasGenerales/Menus/MenuPrincipal.gd
@@ -4,7 +4,6 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	print("Menú Principal cargado")
 
-
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_ESCAPE:
@@ -12,31 +11,37 @@ func _input(event):
 
 func _on_boton_historia_pressed() -> void:
 	print("¡Botón HISTORIA presionado!")
+	
+	# Inicializar modo historia
 	Juego.iniciar_modo_historia()
+	
+	# Ir a la pantalla de transición
 	get_tree().change_scene_to_file("res://EscenasGenerales/Cinematicas/CinematicaGatoDurmiendo.tscn")
 
 func _on_boton_infinito_pressed() -> void:
 	print("¡Botón MODO INFINITO presionado!")
+	
+	# Inicializar modo infinito
 	Juego.iniciar_modo_infinito()
+	
+	# Ir a la pantalla de transición
 	get_tree().change_scene_to_file("res://EscenasGenerales/EscenaVidasNumeroMicrojuego/MicroInicioContadorVidas.tscn")
 
+
+
 func _on_boton_opciones_pressed() -> void:
+
 	print("¡Botón OPCIONES presionado!")
+
 	# 🔧 NUEVO: Cargar el menú de opciones
+
 	get_tree().change_scene_to_file("res://EscenasGenerales/Menus/MenuOpciones.tscn")
 
 func _on_boton_salir_pressed() -> void:
 	print("¡Botón SALIR presionado!")
 	get_tree().quit()
-
-func _on_boton_chat_pressed() -> void:
-	get_tree().change_scene_to_file("res://EscenasGenerales/Menus/Online/user_login.tscn")
-
-
-
+	
 
 func _on_boton_multijugador_pressed() -> void:
-
 	print("¡Botón MULTIJUGADOR presionado!")
-
 	get_tree().change_scene_to_file("res://EscenasGenerales/Menus/Online/user_login.tscn")
